5fa7a02ea49c8f4520f74fa9aa432350b5922bd2
Dylan <c00242050@itcarlow.ie>
2022-03-29 13:18:21 +0100

    Test 15 calcluate rate management kind passed

--- VerificationProject/cm/src/cm/ScottDylanTestTask2.java
+++ VerificationProject/cm/src/cm/ScottDylanTestTask2.java
@@ -380,7 +380,7 @@ public class ScottDylanTestTask2 {
 
         Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, normalPeriods);
 
-        BigDecimal correctVal = BigDecimal.valueOf(15);
+        BigDecimal correctVal = BigDecimal.valueOf(40);
         calc = rate.calculate(periodStay.get(0));
         int returnOF = calc.compareTo(correctVal);
         assertEquals(0, returnOF, "Expected: (" + correctVal + ") Actual: (" + calc + ")");
